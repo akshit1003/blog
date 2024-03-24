1c049ad5776b073fd637e883c74d7064c340f48f
Akshit <akshit10.anand2004@gmail.com>
2024-03-24 11:49:05 +0530

    done ig

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import  express from "express";
 import mongoose from "mongoose";
-import { Product } from "./models/productModel.js";
+// import { Product } from "./models/productModel.js";
+import { Product } from "./models/productModel"
 
 const app = express();
 const port = 8080;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send("API working")
 });
+
 
 app.get('/blog', (req, res) => {
     res.send("Hello blog");
